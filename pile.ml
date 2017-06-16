bfffe14036fe7359d80de47c5928a7ccc05942b8
Rachel Ehrlich <rae84@cornell.edu>
2017-06-16 15:00:18 -0400

    change pile representation

--- pile.ml
+++ pile.ml
@@ -1,21 +1,41 @@
 open Core.Std
-type t = Card.t list
+type t =
+  { top    : Card.t list
+  ; hidden : Card.t list
+  }
 
-let of_cards cards = cards
-
-let print t =
-  List.map t ~f:Card.to_string
+let of_cards cards =
+  match cards with
+  | hd :: tl -> { top = [hd] ; hidden = tl }
+  | [] -> { top = [] ; hidden = [] }
+    
+let print t = 
+  List.map t.top ~f:Card.to_string
+  |> String.concat ~sep:" "
+  |> print_string;
+  print_string " ";
+  List.map t.hidden ~f:Card.to_string
+  |> List.map ~f:(fun s -> "(" ^ s ^ ")")
   |> String.concat ~sep:" "
   |> print_endline
 ;;
 
 let can_play (t : t) (card : Card.t) =
-  match t with
-  | [] -> Card.Value.equal (Card.Value.of_int_exn 13) card.value 
+  match t.top with
+  | [] -> Card.Value.equal (Card.Value.of_int_exn 13) card.value
   | hd :: tl ->
     Card.Value.is_prev card.value hd.value
     && Suit.opposite_colors hd.suit card.suit
 ;;
 
-let top_card = List.hd
-let remove_top_card_exn = List.tl_exn
+let top_card t = List.hd t.top
+let remove_top_card_exn t =
+  match t.top with
+  | [] ->
+    assert (List.is_empty t.hidden);
+    t
+  | [ _hd ] -> 
+    let new_top, new_hidden = List.split_n t.hidden 1 in
+    { top = new_top ; hidden = new_hidden}
+  | _hd :: tl -> { t with top = tl }
+;;
