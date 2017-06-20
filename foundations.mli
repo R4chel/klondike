e924f08bd36c6f6f5c3154c276ba93b815c69ee7
Rachel Ehrlich <rae84@cornell.edu>
2017-06-19 20:09:25 -0400

    remove unnecessary files

--- foundations.mli
+++
@@ -1,9 +0,0 @@
-open Core.Std
-type t
-
-val empty : t
-val playable : t -> Card.t -> bool
-val print : t -> unit 
-val play_if_playable : t -> Card.t -> t 
-val score : t -> int
-
--- pile.mli
+++
@@ -1,9 +0,0 @@
-open Core.Std
-type t
-
-val of_cards : Card.t list -> t 
-val print : t -> unit 
-val top_card : t -> Card.t option
-val remove_top_card_exn : t -> t 
-val can_play : t -> Card.t -> bool
-val play : t -> Card.t -> t 
