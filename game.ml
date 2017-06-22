3db49b00b9cd6872c739eb503a5ea247282ab1ed
Rachel Ehrlich <rae84@cornell.edu>
2017-06-22 15:30:31 -0400

    expirementing

--- game.ml
+++ game.ml
@@ -1,7 +1,7 @@
 open Core.Std
 type t =
   { board : Board.t
-  ; past_actions : Action.t List.t
+  ; consecutive_discards : int
   }
 
 let deal_board board =
@@ -31,26 +31,35 @@ let testing_board i =
  
 let get_action t = Player.get_action t
 
+let end_game (game : t) =
+  Board.score game.board = 52
+  || game.consecutive_discards > 200
+
+
 let turn (game : t) =
   let action = get_action game.board in
-  if Board.valid game.board action then
+  if not (end_game game) && Board.valid game.board action then
     begin
       let board =
         Board.apply_action game.board action 
         |> Board.clean_piles 
       in
-      { board ; past_actions = action :: game.past_actions }
+      match action with
+      | Action.Refresh_deck | Pile_to_pile (Id.Deck, Id.Discard) ->
+        { board ; consecutive_discards = game.consecutive_discards + 1 }
+      | _ ->
+        { board ; consecutive_discards = 0 }
     end
   else game
 
 let () =
-  let num_games = 100 in
-  let wins =
+  let num_games = 1000 in
+  let sum_score =
     List.fold (List.range 0 num_games) ~init:0 ~f:(fun sum i ->
         print_int i; print_newline ();
       (* let board = testing_board i in *)
       let board = new_board () in
-      let game = { board ; past_actions = [] } in
+      let game = { board ; consecutive_discards = 0 } in
       let game =
       List.fold (List.range 0 5000) ~init:game ~f:(fun game _ ->
          turn game 
@@ -58,11 +67,10 @@ let () =
       in
       let score = Board.score game.board in
       if Int.equal score 52 then print_endline ("win!");
-      print_endline ("SCORE " ^ string_of_int score);
+      (* print_endline ("SCORE " ^ string_of_int score); *)
       sum + score
     )
   in
-  print_endline ("SCORE " ^ string_of_int wins);
-  let win_percent = Int.(//) wins num_games in
-  print_endline ("SCORE " ^ string_of_float win_percent);
+  let average_score = Int.(//) sum_score num_games in
+  print_endline ("AVERAGE SCORE " ^ string_of_float average_score);
 ;;
